1fdc983cb19582cfdb044b9b5bfe6e4f5bfd4194
Emmanuel Yirenkyi <emmanuel.addo.yirenkyi@gmail.com>
2020-03-07 04:45:09 +0200

    Modes & Bootstrap Added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
-  belongs_to :user
-  belongs_to :clothing_line
+	belongs_to :user
+	belongs_to :clothing_line
+
+	validates :title, presence: true
+	validates :user_id, presence: true
+	validates :clothing_line_id, presence: true
 end
